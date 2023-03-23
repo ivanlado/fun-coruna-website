9c4b57bfbc4e2fe943b18f001d6c5913361095e4
ivanlado <ivanladoc01@gmail.com>
2023-03-24 00:34:56 +0100

    menu responsivo mas avanzado

--- js/menuResponsivo.js
+++ js/menuResponsivo.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     const btn = document.querySelector(".btn-menu");
-    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    const dropdownMenu = document.querySelector(".nav-items");
+    console.log(dropdownMenu);
     btn.onclick = function () {
-        dropdownMenu.classList.toggle("open");
+        dropdownMenu.classList.toggle("open-menu");
+        const isOpen = dropdownMenu.classList.contains("open-menu");
+        const nuevoDisplay = isOpen ? "auto" : "0px";
+        // dropdownMenu.style.display = nuevoDisplay;
+        dropdownMenu.style.height = nuevoDisplay;
+        // for(e of entradasMenu){
+        //     e.style.display = nuevoDisplay;
+        // }
+
     }
 
 });
