985de919aeb0e23a009f90053f5c953a0cc4e936
ivanlado <ivanladoc01@gmail.com>
2023-03-25 11:28:07 +0100

    testimonios mejorados

--- js/testimonios.js
+++ js/testimonios.js
@@ -3,19 +3,23 @@ let testimonios;
 
 document.addEventListener("DOMContentLoaded", function (event) {
     testimonios = document.getElementsByClassName("testimonio");
-    console.log(testimonios);
     for (i = 0; i < testimonios.length; i++) {
         if (i == indexTestimonio) continue;
         testimonios[i].style.display = "none";
     }
 });
 
-function cambiarTestimonio(direccion) {
+function updateIndex(direccion){
     indexTestimonio += direccion;
     indexTestimonio = indexTestimonio < 0 ? testimonios.length - 1 : indexTestimonio;
     indexTestimonio %= testimonios.length;
+}
+
+function cambiarTestimonio(direccion) {
+    updateIndex(direccion);
     for (tes of testimonios) {
         tes.style.display = "none";
     }
+    
     testimonios[indexTestimonio].style.display = "flex";
 }
