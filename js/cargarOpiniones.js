73c88b8531f8d2e39c8aa8f2f60ef4980616e065
ivanlado <ivanladoc01@gmail.com>
2023-03-29 19:47:32 +0200

    Testimonios

--- js/cargarOpiniones.js
+++ js/cargarOpiniones.js
@@ -1,50 +1,14 @@
 const fileName = "opiniones.xml";
 const filePath = "http://127.0.0.1:5500/xml/" + fileName;
 
-// function cargarOpiniones(){
 
-// }
-
-// $(document).ready(function(){
-//     cargarOpiniones();
-// });
-    // let xhttp = new XMLHttpRequest()
-    // xhttp.onload = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         let json = JSON.parse(this.responseText);
-    //         let code = '';
-    //         // const eventos = json.eventos.filter(evento => evento.fecha === "25 abril");
-    //         const eventos = json.eventos;
-    //         for (e of eventos) {
-    //             code += '<article class="evento"> <div class="container_imagen_evento"> <img src="./images/depobadajoz.jpg" alt="Deportivo vs. Badajoz" class="image_evento img"></div><div class="container_text_evento"><h3 class="nombre_evento">';
-    //             code += e.nombreEvento;
-    //             code += '</h3><p class="fecha-evento"> ';
-    //             code += e.fecha;
-    //             code += '</p><p class="lugar-evento"> ';
-    //             code += e.lugar
-    //             code += '</p><div class="footer-evento"><p class="container-precio"> Desde <span class="precio">';
-    //             code += e.precio;
-    //             code += '€</span></p><p class="btn-reservar" onclick="reservar(this)"> Reservar</p></div></div></article>';
-    //             console.log(code);
-    //         }
-    //         const container = $('#eventos-hoy .caja_eventos');
-    //         console.log(container);
-    //         container.append(code);
-    //     }
-    // }
-    // xhttp.open("GET", filePath);
-    // xhttp.send();
-
-
-
-/*
-<article class="testimonio">
-                    <img src="./images/logoconcello.png" alt="">
-                    <h3>Concello de A Coruña</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum rem repellat voluptatum? Odio
-                        ullam
-                        quidem earum distinctio, obcaecati error minima quo similique sequi culpa nemo ipsum placeat ea
-                        in
-                        numquam?</p>
-                </article>
-*/
+$(document).ready(function(){
+    console.log($(".opiniones-empresas blockquote"));
+    // Empresas
+    $("#opiniones-empresas .presentacion p").load(filePath + " " + "empresa nombre");
+    $("#opiniones-empresas blockquote").load(filePath + " " + "empresa opinion");
+    
+    // Usuarios
+    $("#opiniones-usuarios .presentacion p").load(filePath + " " + "usuario nombre");
+    $("#opiniones-usuarios blockquote").load(filePath + " " + "usuario opinion");
+})
--- js/testimonios.js
+++ js/testimonios.js
@@ -1,23 +1,56 @@
-let indexTestimonio = 0;
-let indexTestimonioAnterior = 0;
-let testimonios;
+let iEmpresas=0;
+let iUsuarios=0;
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    testimonios = document.getElementsByClassName("testimonio");
-    for (i = 0; i < testimonios.length; i++) {
-        if (i == indexTestimonio) continue;
-        testimonios[i].style.display = "none";
-    }
-});
+let indexTestimonioUsuario = 0;
+let testimoniosUsuario;
 
-function updateIndex(direccion) {
-    indexTestimonio += direccion;
-    indexTestimonio = indexTestimonio < 0 ? testimonios.length - 1 : indexTestimonio;
-    indexTestimonio %= testimonios.length;
+const testimoniosEmpresas = "#opiniones-empresas .testimonio";
+const testimoniosUsuarios = "#opiniones-usuarios .testimonio";
+
+const testimoniosEmpresasLen = $(testimoniosEmpresas).length;
+const testimoniosUsuariosLen = $(testimoniosUsuarios).length;
+
+$(testimoniosEmpresas).hide();
+$(testimoniosUsuarios).hide();
+$(testimoniosEmpresas).eq(0).show();
+$(testimoniosUsuarios).eq(0).show();
+
+document.querySelector("#opiniones-empresas .prev").addEventListener("click", slideEmpresasPrev);
+document.querySelector("#opiniones-empresas .next").addEventListener("click", slideEmpresasNext);
+document.querySelector("#opiniones-usuarios .prev").addEventListener("click", slideUsuariosPrev);
+document.querySelector("#opiniones-usuarios .next").addEventListener("click", slideUsuariosNext);
+
+function updateIndex(i, direccion, len) {
+    i += direccion;
+    i = i < 0 ? len - 1 : i;
+    i %= len;
+    return i;
 }
 
-function cambiarTestimonio(direccion) {
-    updateIndex(direccion);
-    $(".testimonio").hide();   
-    $(".testimonio").eq(indexTestimonio).show();  
+function slide(elems, direccion) {
+    if(elems == testimoniosEmpresas){
+        iEmpresas = updateIndex(iEmpresas, direccion, testimoniosEmpresasLen);
+        $(elems).hide();
+        $(elems).eq(iEmpresas).show();
+    } else {
+        iUsuarios = updateIndex(iUsuarios, direccion, testimoniosUsuariosLen);
+        $(elems).hide();
+        $(elems).eq(iUsuarios).show();
+    }
+}
+
+function slideEmpresasPrev(event){
+    slide(testimoniosEmpresas, -1);
+}
+
+function slideUsuariosPrev(evento){
+    slide(testimoniosUsuarios, -1);
+}
+
+function slideEmpresasNext(event){
+    slide(testimoniosEmpresas, 1);
+}
+
+function slideUsuariosNext(evento){
+    slide(testimoniosUsuarios, 1);
 }
