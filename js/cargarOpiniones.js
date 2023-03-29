30c8e9d3410c24383914650c46e0d9e6e731d9fa
ivanlado <ivanladoc01@gmail.com>
2023-03-29 20:43:09 +0200

    wireframe testimonios hecho

--- js/cargarOpiniones.js
+++ js/cargarOpiniones.js
@@ -1,13 +1,10 @@
 const fileName = "opiniones.xml";
 const filePath = "http://127.0.0.1:5500/xml/" + fileName;
 
-
 $(document).ready(function(){
-    console.log($(".opiniones-empresas blockquote"));
     // Empresas
     $("#opiniones-empresas .presentacion p").load(filePath + " " + "empresa nombre");
     $("#opiniones-empresas blockquote").load(filePath + " " + "empresa opinion");
-    
     // Usuarios
     $("#opiniones-usuarios .presentacion p").load(filePath + " " + "usuario nombre");
     $("#opiniones-usuarios blockquote").load(filePath + " " + "usuario opinion");
