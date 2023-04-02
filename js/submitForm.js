18b3ae1fe5c58e4cd57cdcec65462a7ba3a54d1f
ivanlado <ivanladoc01@gmail.com>
2023-04-02 02:13:43 +0200

    submit event

--- js/submitForm.js
+++ js/submitForm.js
@@ -1,10 +1,11 @@
 const email = document.querySelector('input[name="email"]');
 
-document.getElementById("btnSubmit").addEventListener("click", submitReserva);
+document.getElementById("form-reserva").addEventListener("submit", submitReserva);
 const regexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
 
 
 function submitReserva(e) {
+    console.log();
     if (regexEmail.test(email.value)) {
         alert("Se ha enviado la petición de reserva de entradas. En breves recibirán un email para realizar el pago de las mismas.");
     } else {
